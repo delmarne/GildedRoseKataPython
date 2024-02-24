0cc2f20f740f6c158a7c625e2a8ce666047a0e59
delmarne <133262039+delmarne@users.noreply.github.com>
2024-02-23 18:15:21 -0800

    Four new tests

--- gilded_rose.py
+++ gilded_rose.py
@@ -47,3 +47,49 @@ class GildedRose(object):
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+    def get_items_by_name(self, name):
+        return [item for item in self.items if item.name == name]
+
+class Vest:
+    def __init__(self, sell_in, quality):
+        self.name = "+5 Dexterity Vest"
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def update_quality(self):
+        self.quality = self.quality - 1
+        self.sell_in = self.sell_in - 1
+        if self.quality < 0:
+            self.quality = 0
+        if self.sell_in < 0:
+            self.quality = self.quality -2
+                
+    def __eq__(self, other):
+        if isinstance(other, Vest):
+            return (
+                self.name == other.name
+                and self.sell_in == other.sell_in
+                and self.quality == other.quality
+            )
+        return False
+    
+class AgedBrie:
+    def __init__(self, sell_in, quality):
+        self.name = "Aged Brie"
+        self.sell_in = sell_in
+        self.quality = quality
+    
+    def update_quality(self):
+        self.quality = self.quality + 1
+        self.sell_in = self.sell_in - 1
+        if self.quality > 50:
+            self.quality = 50
+
+    def __eq__(self, other):
+        if isinstance(other, AgedBrie):
+            return (
+                self.name == other.name
+                and self.sell_in == other.sell_in
+                and self.quality == other.quality
+            )
+        return False
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import Item, GildedRose, Vest, AgedBrie
 
 
 class GildedRoseTest(unittest.TestCase):
@@ -9,8 +9,39 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
 
+    def test_vest_item_should_decrease_after_one_day(self):
+        items = [Vest(2, 4), Vest(9, 19), Vest(4, 6)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        actual_output = gr.get_items_by_name("+5 Dexterity Vest")
+        expected_output = [Vest(1, 3), Vest(8, 18), Vest(3, 5)]
+        assert actual_output == expected_output
+
+    def test_vest_item_should_decrease_twice_as_fast_after_sell_in(self):
+        items = [Vest(2, 4), Vest(0, 19), Vest(1, 6)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        actual_output = gr.get_items_by_name("+5 Dexterity Vest")
+        expected_output = [Vest(1, 3), Vest(-1, 17), Vest(0, 5)]
+        assert actual_output == expected_output
+        
+    def test_brie_item_should_increase_in_quality(self):
+        items = [AgedBrie(2, 4), AgedBrie(5, 19), AgedBrie(1, 6)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        actual_output = gr.get_items_by_name("Aged Brie")
+        expected_output = [AgedBrie(1, 5), AgedBrie(4, 20), AgedBrie(0, 7)]
+        assert actual_output == expected_output
+
+    def quality_never_higher_than_fifty(self):
+        items = [AgedBrie(5, 50)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        actual_output = gr.get_items_by_name("Aged Brie")
+        expected_output = [AgedBrie(4, 50)]
+        assert actual_output == expected_output
 
 if __name__ == '__main__':
     unittest.main()
